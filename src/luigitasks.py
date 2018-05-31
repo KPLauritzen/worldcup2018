d19a45e2f6bd3c1c7f756c3ef5ca954ccbbe5253
Kasper Primdal Lauritzen <kplauritzen@gmail.com>
2018-05-31 22:56:39 +0200

    Adds merging of ratings and fixtures by date and team

--- src/luigitasks.py
+++ src/luigitasks.py
@@ -4,6 +4,8 @@ from pathlib import Path
 import urllib
 from bs4 import BeautifulSoup as BS
 import pandas as pd
+from datetime import datetime
+import numpy as np
 
 data_dir = Path('../data/').resolve()
 raw_data = data_dir / 'raw'
@@ -11,6 +13,8 @@ processed_data = data_dir / 'processed'
 intermediate_data = data_dir / 'intermediate'
 
 def soup_to_df(soup):
+    date_string = soup.find_all('a', attrs={'class': 'dropdown-toggle'})[-1].contents[0]
+    date = datetime.strptime(date_string, '%b. %d, %Y ')
     team_els = soup.tbody.find_all('td', attrs={'data-title':'Name'})
     team_list = make_list(team_els)
     att_els = soup.tbody.find_all('td', attrs={'data-title':'ATT'})
@@ -27,6 +31,7 @@ def soup_to_df(soup):
     df['MID'] = mid_list
     df['DEF'] = def_list
     df['OVR'] = ovr_list
+    df['Date'] = date.strftime('%Y-%m-%d')
     return df
 
 def make_list(elements):
@@ -38,6 +43,22 @@ def translate_league(league):
         'E0' : '13'
     }
     return trans_dict[league]
+
+def translate_team_name(name):
+    translate_dict = {
+        'Leicester City': 'Leicester',
+        'Manchester City': 'Man City',
+        'Manchester Utd': 'Man United',
+        'Newcastle Utd': 'Newcastle',
+        'Stoke City':'Stoke',
+        'Spurs':'Tottenham'
+    }
+    if not name in translate_dict: return name
+    else:
+        return translate_dict[name]
+
+def translate_season_to_fifa(season):
+    return f'fifa{str(season)[-2:]}'
 
 def download_url(url):
     request = urllib.request.Request(url, headers={'user-agent':'Mozilla/5.0'})
@@ -81,14 +102,19 @@ class DownloadInternationalRatings(luigi.Task):
 class DownloadLeagueRatings(luigi.Task):
     league = luigi.Parameter(default='E0')
     match_day = luigi.Parameter(default='200')
+    season = luigi.Parameter(default='1718')
+
 
     def output(self):
-        path = str(raw_data / f'{self.match_day}/{self.league}.html')
+        path = str(raw_data / f'{self.season}/{self.match_day}/{self.league}.html')
         return luigi.LocalTarget(path=path)
 
     def run(self):
+        fifa_season = translate_season_to_fifa(self.season)
         league_int = translate_league(self.league)
-        url = f'https://www.fifaindex.com/teams/fifa18_{self.match_day}/?league={league_int}'
+        url = f'https://www.fifaindex.com/teams/{fifa_season}_{self.match_day}/?league={league_int}'
+        print()
+        print(url)
         outpath = self.output()
         page = download_url(url)
         outpath.makedirs()
@@ -118,11 +144,12 @@ class ProcessInternationalRatings(luigi.Task):
 class ProcessLeagueRatings(luigi.Task):
     league = luigi.Parameter(default='E0')
     match_day = luigi.Parameter(default='200')
+    season = luigi.Parameter(default='1718')
     def requires(self):
-        return DownloadLeagueRatings(self.league, self.match_day)
+        return DownloadLeagueRatings(self.league, self.match_day, self.season)
 
     def output(self):
-        path = str(intermediate_data / f'{self.match_day}/{self.league}_ratings.csv')
+        path = str(intermediate_data / f'{self.season}/{self.match_day}/{self.league}_ratings.csv')
         return luigi.LocalTarget(path=path)
 
     def run(self):
@@ -130,8 +157,78 @@ class ProcessLeagueRatings(luigi.Task):
             page = f.read()
         soup = BS(page, 'html.parser')
         df = soup_to_df(soup)
+        df.Team = df.Team.apply(translate_team_name)
         self.output().makedirs()
         df.to_csv(self.output().path, index=False)
+
+class MergeLeagueFixturesRatings(luigi.Task):
+    league = luigi.Parameter(default='E0')
+    match_day = luigi.Parameter(default='200')
+    season = luigi.Parameter('1718')
+
+    def requires(self):
+        return [
+            ProcessLeagueRatings(self.league, self.match_day, self.season),
+            DownloadFixtures(self.league, self.season)
+        ]
+
+    def output(self):
+        path = str(intermediate_data / f'{self.season}/{self.match_day}/{self.league}_ratings_fixtures.csv')
+        return luigi.LocalTarget(path=path)
+
+    def run(self):
+        ratings_file, fixtures_file = self.input()
+        df_ratings = pd.read_csv(ratings_file.path)
+        df_fixtures = pd.read_csv(fixtures_file.path)
+
+        # Set dates
+        df_ratings.Date = pd.to_datetime(df_ratings.Date)
+        df_ratings.Date2 = df_ratings.Date + pd.Timedelta(7, unit='D')
+        df_fixtures.Date = pd.to_datetime(df_fixtures.Date)
+        df_ratings['merge_index'] = df_ratings.index - 10000
+
+        # HOME TEAM
+        df_fixtures['index_matched'] = np.piecewise(
+            np.zeros(len(df_fixtures)),
+            [
+                (df_fixtures.Date.values >= start_date) &
+                (df_fixtures.Date.values <= end_date) &
+                (df_fixtures.HomeTeam.values == team)
+                for start_date, end_date, team in
+                zip(df_ratings.Date.values, df_ratings.Date2.values, df_ratings.Team.values)
+             ],
+            df_ratings.merge_index.values
+        )
+
+        df_fixtures = df_fixtures.merge(df_ratings, how='inner', left_on='index_matched', right_on='merge_index')
+        df_fixtures.drop(columns=['index_matched', 'merge_index', 'Team', 'Date_y'], inplace=True)
+        translate_dict = {x:x+'_home' for x in ['ATT', 'DEF', 'MID', 'OVR']}
+        translate_dict.update({'Date_x':'Date'})
+        df_fixtures.rename(columns=translate_dict, inplace=True)
+
+
+        # AWAY TEAM
+        df_fixtures['index_matched'] = np.piecewise(
+            np.zeros(len(df_fixtures)),
+            [
+                (df_fixtures.Date.values >= start_date) &
+                (df_fixtures.Date.values <= end_date) &
+                (df_fixtures.AwayTeam.values == team)
+                for start_date, end_date, team in
+                zip(df_ratings.Date.values, df_ratings.Date2.values, df_ratings.Team.values)
+             ],
+            df_ratings.merge_index.values
+        )
+
+        df_fixtures = df_fixtures.merge(df_ratings, how='inner', left_on='index_matched', right_on='merge_index')
+        df_fixtures.drop(columns=['index_matched', 'merge_index', 'Team', 'Date_y'], inplace=True)
+        translate_dict = {x:x+'_away' for x in ['ATT', 'DEF', 'MID', 'OVR']}
+        translate_dict.update({'Date_x':'Date'})
+        df_fixtures.rename(columns=translate_dict, inplace=True)
+
+        df_fixtures.to_csv(self.output().path, index=False)
+
+
 
 if __name__ == '__main__':
     luigi.run()
